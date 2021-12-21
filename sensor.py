053f231ec9a44b20871e178202215d4c86955fd0
howard <howard.chang@aicpc.com.tw>
2021-12-21 13:59:22 +0800

    add SDR_list.py and sensor.py

---
+++ sensor.py
@@ -0,0 +1,194 @@
+import subprocess
+
+def raw_command(IP, raw):
+    cmd = "ipmitool -I lanplus -H " + IP + " -U admin -P admin raw " + raw
+    # print(cmd)
+    process = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE)
+    #                                ^ if passing a single string, either shell must be True or else the string is not having argument                                        
+    output = process.stdout
+    output = output.read()
+    #print(type(repo))
+    #print(repo)         #byte string
+
+    output = output.strip().decode(encoding = "UTF-8")    #string
+    # print(type(repo))
+    # print(repo + "\n")
+
+    output_list = output.split()
+
+    return output_list
+
+def sensor_ID(sdr):
+
+    ID = ""
+    for index in range(50, len(sdr)):
+        ID += chr(int(sdr[index], 16))
+    return ID
+
+def compact_sensor_ID(sdr):
+    ID = ""
+    for index in range(34, len(sdr)):
+        ID += chr(int(sdr[index], 16))
+    return ID
+
+# analog reading should be converted by SDR factors
+def sensor_reading(IP, sdr):
+    response = raw_command(IP, "0x4 0x2d 0x" + sdr[9])
+
+    converted_reading = analog_convert(sdr, response[0])
+    converted_reading += " " + get_unit(sdr)
+
+    status = get_status(response)
+    if status == "state unavaliable":
+        converted_reading = "no reading"
+
+    return [converted_reading, status]
+
+def analog_convert(sdr, reading):
+
+    x = int(reading, 16)
+    factor_list = sdr_factor(sdr)
+
+    multi = factor_list[0]
+    add = factor_list[1]
+    R_exp = factor_list[2]
+    B_exp = factor_list[3]
+
+    Y = (multi * x + (add * 10 ** B_exp)) * 10 ** R_exp     # exponentiaiton operator is **, not ^
+    return "{:5.2f}".format(Y)
+
+def get_unit(sdr):
+    
+    sensor_unit_1 = int(sdr[22],16)
+    sensor_unit_2 = int(sdr[23],16)
+    sensor_unit_3 = int(sdr[24],16)
+    
+    unit = {
+        0:'unspecified', 
+        1:'degrees C',
+        2:'degrees F',
+        3:'degrees K',
+        4:'Volts',
+        5:'Amps',
+        6:'Watts',
+        7:'Joules',
+        8:'Coulombs',
+        9:'VA',
+        10:'Nits',
+        11:'lumen',
+        12:'lux',
+        13:'Candela',
+        14:'kPa',
+        15:'PSI',
+        16:'Newton',
+        17:'CFM',
+        18:'RPM',
+        19:'Hz',
+        20:'microsecond',
+        21:'millisecond',
+        22:'second',
+        23:'minute',
+        24:'hour',
+        25:'day',
+        26:'week'
+    }
+    return unit.get(sensor_unit_2)
+
+# assume all sensor are theshold sensor
+def get_status(response):
+    
+    if int(response[1], 16) & 32 != 0:
+        return "state unavaliable"
+
+    status = int(response[2], 16) & 63
+    theshold_status = {
+        0:"ok",
+        1:"<= lower non-critical",
+        2:"<= lower critical",
+        4:"<= lower non-recoverable",
+        8:">= upper non-critical",
+        16:">= upper critical",
+        32:">= upper non-recoverable"
+    }
+    return theshold_status.get(status)
+
+def get_sensor_theshold(IP, sdr):
+
+    response = raw_command(IP, "0x4 0x27 0x" + sdr[9])
+    
+    theshold = []
+    helper = 1
+    for i in range(1, 7):
+        if int(response[0], 16) & helper != 0:
+            value = analog_convert(sdr, response[i])
+            theshold.append(value)
+        else:
+            theshold.append("N.A.")
+        helper <<= 1
+    
+    theshold[0], theshold[2] = theshold[2], theshold[0]
+
+    return theshold
+    
+# nominal_reading in SDR is not sensor return value, using Get Sensor Reading to get sensor value
+# Tolerance & Accuracy are not used
+def sdr_factor(sdr):
+
+    M = int(sdr[26], 16)
+    M_T = int(sdr[27], 16)
+    B = int(sdr[28], 16)
+    B_A = int(sdr[29], 16)
+    R_B = int(sdr[31], 16)
+    x = int(sdr[33], 16)
+
+    multi = M + ((M_T >> 6) << 8)
+    add = B + ((B_A >> 6) << 8)
+
+    if R_B >> 7 == 1:
+        R_exp = -(8 - (R_B >> 4) & 7)
+    else:
+        R_exp = R_B >> 4
+    
+    if (R_B >> 3) & 1 == 1:
+        B_exp = -(8 - R_B & 7)
+    else:
+        B_exp = R_B & 7
+    
+    factor_list = [multi, add, R_exp, B_exp]
+
+    return factor_list
+
+# same as ipmitool sensor
+def sensor_print(ID, reading, theshold):
+    print("{:14} | {:<20} | {:<25} | {:<8} | {:<8} | {:<8} | {:<8} | {:<8} | {:<8}".format(
+            ID, reading[0], reading[1], theshold[0], theshold[1], theshold[2], 
+            theshold[3], theshold[4], theshold[5]))
+
+def main():
+    # IP = input("Enter IP: ")
+    IP = "192.168.22.51"
+    repo = raw_command(IP, "0xa 0x22")              # get repository only once!
+    repo = "0x" + repo[0] + " " + "0x" + repo[1]
+    cmd = "0xa 0x23 " + repo + " 0x0 0x0 0x0 0xff"
+
+    print("{:14} | {:<20} | {:<25} | {:<8} | {:<8} | {:<8} | {:<8} | {:<8} | {:<8}".format(
+            "ID", "sensor reading", "status", "l.n-r.t.", "l.c.t.", "l.n-c.t.", 
+            "u.n-c.t.", "u.c.t.", "u.n-r.t."))
+    print("{:{c}^{n}}".format("", c = "-", n = 131))        
+    sdr = raw_command(IP, cmd)
+    while sdr[0] != "ff" and sdr[1] != "ff":        # FFFFh specifies that the last SDR should be listed
+        if sdr[5] == "01":                          # get full sensor only
+            ID = sensor_ID(sdr)
+            reading = sensor_reading(IP, sdr)
+            theshold = get_sensor_theshold(IP, sdr)
+            sensor_print(ID, reading, theshold)
+        if sdr[5] == "02":                          # get compact sensor
+            ID = compact_sensor_ID(sdr)
+            reading = sensor_reading(IP, sdr)
+            theshold = ["N.A.", "N.A.", "N.A.", "N.A.", "N.A.", "N.A."]
+            sensor_print(ID, reading, theshold)
+
+        cmd = "0xa 0x23 " + repo + " 0x" + sdr[0] + " 0x" + sdr[1] + " 0x0 0xff"
+        sdr = raw_command(IP, cmd)
+
+main()
